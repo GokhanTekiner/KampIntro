006b2bc566f0803c6712d96ed28079d8ee4273b2
Gökhan <tekiner07@gmail.com>
2021-01-20 14:28:51 +0300

    Odev

---
+++ Odev/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer() { Id = 1, FirstName = " Gökhan ", LastName = "Tekiner ", City = "Antalya" };
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Delete(customer);
+
+        }
+                
+    }
+
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }   
+        
+    }
+
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Id + customer.FirstName + customer.LastName + customer.City);
+            Console.WriteLine("Müşteri eklendi!");
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Id + customer.FirstName + customer.LastName + customer.City);
+            Console.WriteLine("Müşteri Silindi!");
+        }
+    }
+
+    
+}
